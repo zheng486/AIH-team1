150107a060196765f0fb9c36d47a54282b6fcb5a
zheng486 <42808212+zheng486@users.noreply.github.com>
2021-05-01 09:38:51 -0400

    Add files via upload

--- maxsub.py
+++ maxsub.py
@@ -1,26 +1,19 @@
 # create and evaluate a static autoregressive model
 from pandas import read_csv
 from datetime import datetime
-
-from statsmodels.tsa.ar_model import AutoReg
-from sklearn.metrics import mean_squared_error
-from math import sqrt
 import numpy as np
-import pandas_datareader as pdr
 import pandas as pd
-import seaborn as sns
-from statsmodels.tsa.ar_model import AutoReg, ar_select_order
-from statsmodels.tsa.api import acf, pacf, graphics
+
 
 import matplotlib.pyplot as plt
 # load dataset
 #frame = pd.read_csv("./audio/audio_u00.csv",skiprows=854360, nrows = 5)
-frame = pd.read_csv("./audio/audio_u02.csv")
+frame = pd.read_csv("./audio/audio_u02.csv") #the path of the csvfile
 bt_data = frame.to_numpy()
-print(len(frame))
+print("csv file length:",len(frame))
 #print(frame)
-level = []
-time = []
+level = []      #storing the audio inference
+time = []       #storing the time
 start  = 0
 end = 0
 count = 0
@@ -28,42 +21,40 @@ max = 0
 
 for i in range (len(bt_data)):
     level.append(bt_data[i][1])
-    #if (i < )
     time.append(bt_data[i][0])
 
-#print(frame.iloc[854361])
-#print(frame.iloc[2549557])
 
 each_value,each_value_count = np.unique(level,return_counts = True)
-print(each_value)
-print(each_value_count)
+print("all levels:", each_value)            #all levels
+print("l number:",each_value_count)         #the number of each level
+print()
 
 j = 0
 while j < len(bt_data):
-    if (level[j] == 0):
+    if (level[j] == 1):             #the user need to change this part as the audio inference you want
         #print("j",j)
         k=j
         count = 0
-        while k<len(bt_data) and level[k] == 0:
+        while k<len(bt_data) and level[k] == 1:         #the user need to change this part as the audio inference you want
             #print("k" ,k, "level ", level[k])
             count +=  1
             k+=1
         if(max < count):
             max = count
-            #start = time[j]
-            #end = time[j+count-1]
-            start = j
-            end = k
+            start = j       #the index of the start
+            end = k         #the index of the end
         j=k+1  
-        #count = 0
     else: j += 1
 print("Length:" ,max)
 print("Start position:",start, "Start time: ", time[start])
 print("End position:" ,end, "End time: ", time[end])
 
 
-print(frame.iloc[start-1:end+1])
+print()
+print("The test part:")
+print(frame.iloc[start-1:end+1])        #The data in the period we get
 
+#the distribution of the level in this period
 each_value_test,each_value_count_test = np.unique(frame.iloc[start-1:end+1,1:2],return_counts = True)
 print(each_value_test)
 print(each_value_count_test)
